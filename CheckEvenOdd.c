12d1c178c8071e710529dc59f3523d628a317e8e
Sumangal44 <123644743+Sumangal44@users.noreply.github.com>
2024-07-28 19:23:29 +0530

    Create CheckEvenOdd.c

---
+++ CheckEvenOdd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+int n ;
+  printf("enter the number ");
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+    printf("%d the number is even ",n);
+  }
+  else
+  {
+    printf("%d the number is odd",n);
+  }
+}
