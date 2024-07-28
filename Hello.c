e51317bcd65b38913949e177d7c0feaf8b18e728
Sumangal44 <123644743+Sumangal44@users.noreply.github.com>
2024-07-28 19:21:28 +0530

    Create Hello.c

---
+++ Hello.c
@@ -0,0 +1,12 @@
+// Simple C program to display "Hello World"
+ 
+// Header file for input output functions
+#include <stdio.h>
+
+// main function -
+// where the execution of program begins
+void main()
+{
+    // prints hello world
+    printf("Hello World");
+}
